e6a5771237f6cc2dee53b65a06c81ed6f53e0933
Emerson04-sv <balmore04ebao@gmail.com>
2021-06-08 12:36:27 -0600

    Creé el login de la pagina

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hola");
+const login=document.getElementById("login");
+const username=document.getElementById("username");
+const messageLogin=document.getElementById("messageLogin");
+const loginEnter=document.getElementById("btn-login");
+
+let isLogin=false;
+
+loginEnter.addEventListener("click", function(){
+    emptyImput(username.value);
+});
+
+function emptyImput(name){
+    if(name){
+        isLogin=!isLogin;
+        nextPage1(isLogin);
+    }
+    else{
+        messageLogin.classList.replace("visible-off","visible-on");
+    }
+}
+function nextPage1 (click){
+    if(click){
+        login.classList.replace("visible-on", "visible-off");
+    }
+}
