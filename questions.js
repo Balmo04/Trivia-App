84667971dbef2528f4ec914703fe92f64658b785
Emerson04-sv <balmore04ebao@gmail.com>
2021-06-19 15:34:24 -0600

    Hice objetos para el modo facil y el dificil

--- questions.js
+++ questions.js
@@ -1,8 +1,91 @@
-class question {
-    constructor (numQuestion, ansG, ansB){
-        
+const createQuestion = (questionList, answerList, goodAnswerList) =>{
+    const structure ={
+        questions: questionList,
+        answers: answerList,    
+        correctAnswer: goodAnswerList,
     }
-    /*Metodos del objeto*/
+    return structure;
 }
 
-/*Aqui preguntas*/
+/*Modo facil*/
+const arrayQuestionE=[
+    "¿Cuándo empezó la Primera Guerra Mundial?",
+    "¿Cuánto vale el número pi?",
+    "¿Cuántas patas tiene la araña?",
+    "¿Cual es país más poblado de la Tierra?",
+    "¿Cuál es el metal más caro del mundo?",
+    "¿En qué año comenzó la II Guerra Mundial?",
+    "El triángulo que tiene sus tres lados iguales", 
+    "¿Cuántos corazones tienen los pulpos?",
+    "¿Quién ganó el mundial de 2014?",
+    "¿Dónde se encuentra la famosa Torre Eiffel?",
+];
+const arrrayGoodAnswerE=[
+    "1914",
+    "3.1416",
+    "8",
+    "China",
+    "Rodio",
+    "1939",
+    "Equilatero",
+    "3",
+    "Brazil",
+    "Francia",
+];
+const arrayAnswerE=[
+    ["1914","1916", "1920", "1917"],
+    ["3.1416","3.1415", "3.1817", "3.1516"],
+    ["8","6", "12", "10"],
+    ["China","Japón","EE.UU","España"],
+    ["Rodio","Oro","Plata","Cobre"],
+    ["1939","1932","1949","1930"],
+    ["Equilatero","Cuadrilátero","Isosceles","Escaleno"],
+    ["3","1","2","4"],
+    ["Brazil","Colombia","Argentina","Chile"],
+    [ "Francia","Ecuador","Perú","España"],
+];
+
+/*Modo dificil*/
+const arrayQuestionH=[
+    "¿Quién pintó “la última cena”?",
+    "¿Qué año llegó Cristóbal Colón a América?",
+    "¿Cuántos años tiene un lustro?",
+    "¿Cuál es el lugar más frío de la tierra?",
+    "¿Cuál es el metal más caro del mundo?",
+    "¿Cuál es el río más largo del mundo?",
+    "¿Quién escribió La Odisea?", 
+    "¿Cuál es la escritura utilizada por las personas con discapacidad visual ?",
+    "¿Mozart era un genio del mundo de...?",
+    "¿Cuál es el quinto planeta en el sistema solar?",
+];
+const arrrayGoodAnswerH=[
+    "Leonardo da Vinci",
+    "1492",
+    "5 años",
+    "La Antártida",
+    "Rodio",
+    "El Nilo",
+    "Homero",
+    "Braile",
+    "LA música",
+    "Júpiter",
+];
+const arrayAnswerH=[
+    ["Miguel Angel","Rafael Sanzio", "Leonardo da Vinci", "Vincent van Gogh"],
+    ["1522","1492", "1942", "1512"],
+    ["100 años","10 años", "5 años", "50 años"],
+    ["La Antártida","La Patagonia","El Polo Norte","Rusia"],
+    ["Rodio","Oro","Plata","Cobre"],
+    ["El amazonas","El Nilo","El Éufrates","Misisipi"],
+    ["Pláton","Homero","Virgilio","Sócrates"],
+    ["Jeroglifico","Castellano","Morce","Braile"],
+    ["La música","La opera","El deporte","La medicina"],
+    ["Saturno","Marte","Júpiter","Urano"],
+];
+
+
+const easyMode= createQuestion(arrayQuestionE, arrayAnswerE, arrrayGoodAnswerE);
+const hardMode= createQuestion(arrayQuestionH, arrayAnswerH, arrrayGoodAnswerH);
+
+console.log(easyMode);
+console.log(hardMode);
